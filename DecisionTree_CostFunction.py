62c686d4b8625c7540def06924cd365767083d1d
Fay2015 <fay.tang@gmail.com>
2015-11-13 17:05:52 -0500

    modeling cost function

--- DecisionTree_CostFunction.py
+++ DecisionTree_CostFunction.py
@@ -1,4 +1,5 @@
 import collections
+from PIL import Image,ImageDraw
 
 class decisionnode(object):
   def __init__(self, col=-1, value=None, results=None, tb=None, fb=None):
@@ -187,19 +188,61 @@ def testdata():
   return [map(cleanup, line.split('\t'))
       for line in open('trainingDataWithWeight.txt')]
 
+def getwidth(tree):
+  if tree.tb==None and tree.fb==None: return 1
+  return getwidth(tree.tb)+getwidth(tree.fb)
+  
+def getdepth(tree):
+  if tree.tb==None and tree.fb==None: return 0
+  return max(getdepth(tree.tb),getdepth(tree.fb))+1
+
+def drawtree(tree,jpeg='tree.jpg'):
+  w=getwidth(tree)*100
+  h=getdepth(tree)*100+120
+
+  img=Image.new('RGB',(w,h),(255,255,255))
+  draw=ImageDraw.Draw(img)
+
+  drawnode(draw,tree,w/2,20)
+  img.save(jpeg,'JPEG')
+
+def drawnode(draw,tree,x,y):
+  if tree.results==None:
+    # Get the width of each branch
+    w1=getwidth(tree.fb)*100
+    w2=getwidth(tree.tb)*100
+
+    # Determine the total space required by this node
+    left=x-(w1+w2)/2
+    right=x+(w1+w2)/2
+
+    # Draw the condition string
+    draw.text((x-20,y-10),str(tree.col)+':'+str(tree.value),(0,0,0))
+
+    # Draw links to the branches
+    draw.line((x,y,left+w1/2,y+100),fill=(255,0,0))
+    draw.line((x,y,right-w2/2,y+100),fill=(255,0,0))
+
+    # Draw the branch nodes
+    drawnode(draw,tree.fb,left+w1/2,y+100)
+    drawnode(draw,tree.tb,right-w2/2,y+100)
+  else:
+    txt=' \n'.join(['%s:%d'%v for v in tree.results.items(  )])
+    draw.text((x-20,y),txt,(0,0,0))
 
 if __name__ == '__main__':
 
   tree = buildtree(testdata())
   #print "print tree"
   #printtree(tree)
-  print " "
+  print ""
   prune(tree, 0.1)
-  print "Decision Tree:"
+  print "Decision Tree"
   printtree(tree)
+  print ""
+  print "classify [300, 370, 70]  flight Fare $300, flight duration 370 minutes, ground travel time 70 minutes"
+  print classify([300, 370, 70], tree)
   #prune(tree, 1.0)
   #print "prune tree, 1.0"
   #printtree(tree)
-  print " "
-  print "classify [300, 370, 70]  flight Fare $300, flight duration 370 minutes, ground travel time 70 minutes:"
-  print classify([300, 370, 70], tree)
+  drawtree(tree,jpeg='DecisionTree.jpg')

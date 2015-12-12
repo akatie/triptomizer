466d2d2cb5a9cd6e1947c9e376952aa80fa410a1
Fay2015 <fay.tang@gmail.com>
2015-12-11 23:52:04 -0500

    12/11/2015

---
+++ getTrainTestData.sql
@@ -0,0 +1,8 @@
+﻿SELECT DISTINCT ON (totalcost)
+       id, totalcost, totalduration, airport, flightid, flightcost, 
+       parkingcost, drivingcost, timeleavehome, flightdeparture, flightduration, 
+       atairporttime, drivingduration, airline
+FROM   computations_12102015
+ORDER  BY totalcost, totalduration, airport, flightid, flightcost, 
+       parkingcost, drivingcost, timeleavehome, flightdeparture, flightduration, 
+       atairporttime, drivingduration, airline, id;
